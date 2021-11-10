b7e8e411ea6159e2d3cd7075f8f8eb05a5fe195d
davidmorosini <david.morosinineto@gmail.com>
2021-11-10 20:11:49 -0300

    fix: sonar bugs

--- rdict/__init__.py
+++ rdict/__init__.py
@@ -3,7 +3,7 @@ from rdict.utils import array_keys_to_path, path_to_array_keys
 
 
 __all__ = [
-    Rdict,
-    array_keys_to_path,
-    path_to_array_keys,
+    "Rdict",
+    "array_keys_to_path",
+    "path_to_array_keys",
 ]
